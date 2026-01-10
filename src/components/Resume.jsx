50a0c28be8f69c4692cf0154c1c5243cd117d57e
Nnange <awong.nnange@yahoo.com>
2026-01-10 23:26:21 +0100

    updated the work, education and skilss sections

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,93 +1,116 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+
 export default class Resume extends Component {
+  static propTypes = {
+    resumeData: PropTypes.shape({
+    work: PropTypes.arrayOf(
+      PropTypes.shape({
+        CompanyName: PropTypes.string,
+        specialization: PropTypes.string,
+        MonthOfLeaving: PropTypes.string,
+        YearOfLeaving: PropTypes.string,
+        Tasks: PropTypes.arrayOf(PropTypes.string),
+      })
+    ),
+    education: PropTypes.arrayOf(
+      PropTypes.shape({
+        UniversityName: PropTypes.string,
+        specialization: PropTypes.string,
+        MonthOfPassing: PropTypes.string,
+        YearOfPassing: PropTypes.string,
+        Achievements: PropTypes.string,
+        duration: PropTypes.string,
+      })
+    ),
+    }),
+    };
+
+
+  DisplayMonthYear(a) {
+    if (a === "Present") return "Present";
+    const date = new Date(a);
+    const month = date.toLocaleString("default", { month: "short" });
+    const year = date.getFullYear();
+    return `${month} ${year} `;
+  } 
+
+  getDuration(startDate, endDate) {
+    const start = new Date(startDate);
+    let end;
+    if (endDate === "Present") {
+      end = new Date();
+    } else {
+      end = new Date(endDate);
+    }
+    const months =
+      (end.getFullYear() - start.getFullYear()) * 12 +
+      (end.getMonth() - start.getMonth()) + 1;
+      if (months > 12) {
+        const years = Math.floor(months / 12);
+        const remMonths = months % 12;
+        if (remMonths === 0) {
+          return `${years} yr${years > 1 ? "s" : ""}`;
+        }
+        return `${years} yr${years > 1 ? "s" : ""} ${remMonths} mo${remMonths > 1 ? "s" : ""}`;
+      }
+    return `${months} mo${months > 1 ? "s" : ""}`;
+  }
   render() {
     let resumeData = this.props.resumeData;
 
-    function DisplayMonthYear(a) {
-      if (a === "Present") return "Present";
-      const date = new Date(a);
-      const month = date.toLocaleString("default", { month: "short" });
-      const year = date.getFullYear();
-      return `${month} ${year} `;
-    } 
-
-    function getDuration(startDate, endDate) {
-      const start = new Date(startDate);
-      let end;
-      if (endDate === "Present") {
-        end = new Date();
-      } else {
-        end = new Date(endDate);
-      }
-      const months =
-        (end.getFullYear() - start.getFullYear()) * 12 +
-        (end.getMonth() - start.getMonth()) + 1;
-        if (months > 12) {
-          const years = Math.floor(months / 12);
-          const remMonths = months % 12;
-          if (remMonths === 0) {
-            return `${years} yr${years > 1 ? "s" : ""}`;
-          }
-          return `${years} yr${years > 1 ? "s" : ""} ${remMonths} mo${remMonths > 1 ? "s" : ""}`;
-        }
-      return `${months} mo${months > 1 ? "s" : ""}`;
-    }
-      
   
     return (
-      <section id="resume">
+      <section id="resume" className="bg-white text-gray-800 py-20">
         
-        <div className="row work">
-          <div className="three columns header-col">
-            <h1>
-              <span>Work</span>
-            </h1>
+        <div className="grid grid-cols-12 gap-6 py-7 w-3/4 mx-auto">
+          <div className="col-span-3">
+            <span className="font-bold text-2xl border-b-4 border-blue-500">Work</span>
           </div>
 
-          <div className="nine columns main-col">
-            {resumeData.work &&
-              resumeData.work.map((item, i) => {
+          <div className="col-span-9">
+            { resumeData.work.map((item, i) => {
                 return (
-                  <div className="row item" key={i}>
-                    <div className="twelve columns">
-                      <h3>{item.specialization}</h3>
-                      <p className="info">
+                  <div className="" key={i}>
+                    <div className="">
+                      <span className="font-bold text-3xl">{item.specialization}</span>
+                      <p className="text-lg mb-2">
                         {item.CompanyName}
                         <span>&bull;</span>{" "}
                         <em className="date">
-                          { DisplayMonthYear(item.MonthOfLeaving) }
-                          – {DisplayMonthYear(item.YearOfLeaving)} • {getDuration(item.MonthOfLeaving, item.YearOfLeaving)}
+                          { this.DisplayMonthYear(item.MonthOfLeaving) }
+                          – {this.DisplayMonthYear(item.YearOfLeaving)} • {this.getDuration(item.MonthOfLeaving, item.YearOfLeaving)}
                         </em>
                       </p>
-                      <i>
+                      <i className="">
                         { item.Tasks.map((i) => {
-                            return <span key={i}> {i} <br /> </span>
+                            return <span key={i} className="leading-7"> {i} <br /> </span>
                           }) 
                         }
-                        <hr />
                       </i>
                     </div>
+                    <hr className="my-6 border border-gray-400" />
                   </div>
                 );
               })}
           </div>
         </div>
 
-        <div className="row education">
-          <div className="three columns header-col">
-            <h1>
-              <span>Education</span>
-            </h1>
+        <hr className="my-6 border border-gray-400" />
+
+        <div className="grid grid-cols-12 gap-6 py-7 w-3/4 mx-auto">
+          <div className="col-span-3">
+            <span className="font-bold text-2xl border-b-4 border-blue-500">Education</span> 
           </div>
 
-          <div className="nine columns main-col">
-            {resumeData.education &&
-              resumeData.education.map((item, i) => {
+          <div className="col-span-9">
+            { resumeData.education.map((item, i) => {
                 return (
                   <div className="row item" key={i}>
                     <div className="twelve columns">
-                      <h3>{item.UniversityName}</h3>
-                      <p className="info">
+                      <span className="font-bold text-3xl">{item.UniversityName}</span>
+                      <p className="text-md mb-2">
                         {item.specialization}
                         <span>&bull;</span>{" "} 
                         <em className="date">
@@ -96,22 +119,24 @@ export default class Resume extends Component {
                       </p>
                       <span>{item.Achievements}</span> 
                     </div>
-                    <hr />
+                    <hr className="my-6 border border-gray-400" />
                   </div>
                 );
               })}
           </div>
         </div>
 
-        <div className="row skill">
-          <div className="three columns header-col">
+        <hr className="my-6 border border-gray-400" />
+
+        <div className="grid grid-cols-12 gap-6 py-7 w-3/4 mx-auto">
+          <div className="col-span-3">
             <h1>
-              <span>Skills</span>
+              <span className="font-bold text-2xl border-b-4 border-blue-500">Skills</span>
             </h1>
           </div>
 
-          <div className="nine columns main-col">
-            <div className="">
+          <div className="col-span-9">
+            <div className="flex flex-col space-y-4">
               <ul className="">
                 <strong>Languages:</strong> English (Native), German (B2), French (fluent)
               </ul>
